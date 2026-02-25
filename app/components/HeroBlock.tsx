4ea1aac5aaeee4244a00a9d15cbff179f65e2ccb
Rodrigo Baron <baron.rodrigo0@gmail.com>
2026-02-24 23:27:12 -0300

    change logo
    Signed-off-by: Rodrigo Baron <baron.rodrigo0@gmail.com>

--- app/components/HeroBlock.tsx
+++ app/components/HeroBlock.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export function HeroBlock() {
   return (
     <div className='hero-block'>
-      <Image src='/images/general/vibe-notes.png' alt='Vibe Notes' width={400} height={400} className='h-fit' />
+      <Image src='/images/general/vibe-notes.svg' alt='Vibe Notes' width={400} height={400} className='h-fit' />
       <span className='hero-block-text'>
         Machine Learning Developer Notes — Building, Optimizing, Serving, and Scaling
       </span>
