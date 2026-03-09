5ee636199106f935ac207cdbd04238b6004b8e88
Rodrigo Baron <baron.rodrigo0@gmail.com>
2026-03-09 11:10:27 -0300

    make things smaller
    Signed-off-by: Rodrigo Baron <baron.rodrigo0@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const navbar = (
             <div className='logo'>
             <Image src='/images/general/logo.svg' alt='logo' width={40} height={40} className='h-fit' />
             <span className='logo-text'>
-                Rodrigo Baron's VibeNotes
+                Baron's VibeNotes
             </span>
             </div>
         )}
@@ -26,7 +26,7 @@ const navbar = (
 const footer = (
     <Footer>
         <div className='footer-inner'>
-            <span className='footer-logo-text'>Rodrigo Baron&apos;s VibeNotes</span>
+            <span className='footer-logo-text'>Baron&apos;s VibeNotes</span>
             <span className='footer-copy'>
                 © <time>{new Date().getFullYear()}</time> Rodrigo Baron
             </span>
