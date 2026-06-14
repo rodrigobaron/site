c938538e84fb498879c9d6d4d7e8055ebc36c9d5
Rodrigo Baron <baron.rodrigo0@gmail.com>
2026-06-14 16:55:50 -0300

    remove old stuff
    Signed-off-by: Rodrigo Baron <baron.rodrigo0@gmail.com>

--- app/components/ThemedImage.tsx
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-import Image from 'next/image'
-
-export function ThemedImage({ className = '', ...props }) {
-  return <Image {...props} className={`themed-image ${className}`.trim()} />
-}
